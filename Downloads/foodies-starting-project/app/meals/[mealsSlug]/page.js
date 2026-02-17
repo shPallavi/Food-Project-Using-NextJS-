7b96fb745697041b8c147da8743cdd41ba6cd7fe
shPallavi <singhhhpallavi@gmail.com>
2026-02-17 11:39:18 +0530

    Initial commit

---
+++ Downloads/foodies-starting-project/app/meals/[mealsSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealsDetailsPage() {
+  return (
+    <>
+      <h1>Meals Detail</h1>
+    </>
+  );
+}
---
+++ Downloads/foodies-starting-project/app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function ShareMealsPage() {
+  return (
+    <>
+      <main>
+        <h1>Share Meals</h1>
+      </main>
+    </>
+  );
+}

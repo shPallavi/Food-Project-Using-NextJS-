fb230b1ed706c549a92776816b900f9152ef2bac
shPallavi <singhhhpallavi@gmail.com>
2026-02-17 11:55:07 +0530

    Initial commit

---
+++ components/main-header/main-header.js
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="Picture of the author" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals"> Browser Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community"> Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}

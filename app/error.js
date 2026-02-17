c47535f35e419a1d789cf336bf2be7a71e68092d
shPallavi <singhhhpallavi@gmail.com>
2026-02-17 15:16:01 +0530

    Add error handling pages

---
+++ app/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Error() {
+  return (
+    <>
+      <main className="error">
+        <h1>An Error occured!</h1>
+        <p>Failed to Fetch meals data. Please try again later.</p>
+        {/* <p>{error}</p> */}
+      </main>
+    </>
+  );
+}
